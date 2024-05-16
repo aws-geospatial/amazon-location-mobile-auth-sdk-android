4e781a2e1dd49523a043c36b107fd006aea6649b
shah <shah272728@gmail.com>
2024-05-16 15:07:25 +0530

    feat: code optimization
    ALMS-125

--- library/src/main/java/software/amazon/location/auth/data/response/GetCredentialResponse.kt
+++ library/src/main/java/software/amazon/location/auth/data/response/GetCredentialResponse.kt
@@ -2,8 +2,6 @@ package software.amazon.location.auth.data.response
 
 import com.google.gson.annotations.SerializedName
 
-
-
 data class GetCredentialResponse(
     @SerializedName("Credentials") val credentials: Credentials,
     @SerializedName("IdentityId") val identityId: String
--- library/src/main/java/software/amazon/location/auth/data/response/GetIdentityIdResponse.kt
+++ library/src/main/java/software/amazon/location/auth/data/response/GetIdentityIdResponse.kt
@@ -2,7 +2,6 @@ package software.amazon.location.auth.data.response
 
 import com.google.gson.annotations.SerializedName
 
-
 data class GetIdentityIdResponse(
     @SerializedName("IdentityId") val identityId: String
 )
