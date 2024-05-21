4d192b0832bc3369cef5160a78d595b597352b33
shah <shah272728@gmail.com>
2024-05-21 20:05:57 +0530

    feat: updated AwsSignerInterceptor to work with map load
    ALMS-149

--- library/src/main/java/software/amazon/location/auth/utils/Constants.kt
+++ library/src/main/java/software/amazon/location/auth/utils/Constants.kt
@@ -4,6 +4,7 @@ package software.amazon.location.auth.utils
 object Constants {
     const val URL = "https://cognito-identity.%s.amazonaws.com/"
     const val MEDIA_TYPE = "application/x-amz-json-1.1"
+    const val SIGNING_ALGORITHM = "AWS4-HMAC-SHA256"
     const val HEADER_X_AMZ_TARGET = "X-Amz-Target"
     const val TIME_PATTERN = "yyyyMMdd'T'HHmmss'Z'"
     const val HEADER_X_AMZ_DATE = "x-amz-date"
@@ -19,4 +20,5 @@ object Constants {
     const val METHOD= "method"
     const val IDENTITY_POOL_ID= "identityPoolId"
     const val API_KEY = "apiKey"
+    const val DEFAULT_ENCODING = "UTF-8"
 }
