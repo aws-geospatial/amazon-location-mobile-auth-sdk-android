ac9363e228fd900fd25daa7827d6d9f421503976
shah <shah272728@gmail.com>
2024-06-10 17:56:06 +0530

    feat: removed amplify from sample app
    ALMS-147

--- library/src/main/java/software/amazon/location/auth/utils/Constants.kt
+++ library/src/main/java/software/amazon/location/auth/utils/Constants.kt
@@ -9,6 +9,7 @@ object Constants {
     const val HEADER_X_AMZ_SECURITY_TOKEN = "x-amz-security-token"
     const val HEADER_X_AMZ_CONTENT_SHA256= "x-amz-content-sha256"
     const val HEADER_AUTHORIZATION= "authorization"
+    const val IDENTITY_ID= "identityId"
     const val ACCESS_KEY_ID= "accessKeyId"
     const val SECRET_KEY= "secretKey"
     const val SESSION_TOKEN= "sessionToken"
