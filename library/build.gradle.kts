a5d01c0aa9f2d9cae54ca7ffbb01a6a447df62ba
Evan Boucher <24446562+ejboucher@users.noreply.github.com>
2024-06-17 16:04:50 -0500

    Remove automatic Maven step

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,42 +1,6 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("com.vanniktech.maven.publish") version "0.27.0"
-}
-
-mavenPublishing {
-    publishToMavenCentral(SonatypeHost.DEFAULT, automaticRelease = true)
-    signAllPublications()
-
-    coordinates("software.amazon.location", "auth", "0.0.2")
-
-    pom {
-        name.set("My Library")
-        description.set("A description of what my library does.")
-        inceptionYear.set("2020")
-        url.set("https://github.com/username/mylibrary/")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-        developers {
-            developer {
-                id.set("username")
-                name.set("User Name")
-                url.set("https://github.com/username/")
-            }
-        }
-        scm {
-            url.set("https://github.com/username/mylibrary/")
-            connection.set("scm:git:git://github.com/username/mylibrary.git")
-            developerConnection.set("scm:git:ssh://git@github.com/username/mylibrary.git")
-        }
-    }
 }
 
 android {
